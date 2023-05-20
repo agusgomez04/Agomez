6d5abd0214ae37374ac82c87aeaef54c20a7fa54
agusgomez04 <agusgomez0409@outlook.com>
2023-05-19 22:29:37 -0300

    Agrego loggers

--- agom/src/main/java/com/agom/agom/controller/ProductoController.java
+++ agom/src/main/java/com/agom/agom/controller/ProductoController.java
@@ -3,7 +3,10 @@ package com.agom.agom.controller;
 import com.agom.agom.model.Producto;
 import com.agom.agom.service.jpa.ProductoServiceImpl;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,8 @@ public class ProductoController {
 
     @Autowired
     ProductoServiceImpl service;
+
+    private static final Logger logger = LogManager.getLogger(ProductoController.class);
 
     @GetMapping()
     public ResponseEntity<List<Producto>> getAllProductos() {
@@ -35,6 +40,7 @@ public class ProductoController {
             Producto producto = service.findById(productoId);
             return new ResponseEntity<>(producto, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
+            logger.error("[getProductoById] Error al obtener el producto", e);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -45,7 +51,11 @@ public class ProductoController {
             Producto producto = service.findByNombre(nombre);
             return new ResponseEntity<>(producto, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
+            logger.error("[getProductoByName] Error al obtener el producto", e);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (IncorrectResultSizeDataAccessException irsdae){
+            logger.info("[getProductoByName] Existe más de un producto con ese nombre", irsdae);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -55,6 +65,7 @@ public class ProductoController {
             Producto productoCreated = service.save(producto);
             return new ResponseEntity<>(productoCreated, HttpStatus.CREATED);
         }catch (IllegalArgumentException e){
+            logger.error("[createProducto] Error al crear el producto", e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -68,8 +79,10 @@ public class ProductoController {
             Producto updatedProducto = service.update(producto);
             return new ResponseEntity<>(updatedProducto,HttpStatus.OK);
         }catch (IllegalArgumentException ile){
+            logger.error("[updateProducto] Error al actualizar el producto", ile);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }catch (InputMismatchException ipm){
+            logger.error("[updateProducto] Error al actualizar el producto", ipm);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -80,6 +93,7 @@ public class ProductoController {
             service.deleteById(productoId);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (IllegalArgumentException e) {
+            logger.error("[deleteProductoById] Error al eliminar el producto", e);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
